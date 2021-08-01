3b2001440c39f842b3cd9744ec5581577db34473
Sarafat Murshid <sarafatmurshid@gmail.com>
2021-08-01 12:37:42 +0600

    ajax

---
+++ Project/adminlogin.php
@@ -0,0 +1,28 @@
+<?php include 'controllers/admincontroller.php';?>
+ <html>
+    <head>
+	<title>Login</title>
+	</head>
+	<body>
+	    <div align="center">
+		    <h1>Login</h1>
+			<h5><?php echo $err_db; ?></h5>
+			<form action="" method="post">
+			    <div align="center">
+				    <h4>Username</h4>
+					<input type="text" name="uname" value="<?php echo $uname; ?>"/>
+					<span> <?php echo $err_uname; ?> </span>
+				</div>
+				<div align="center">
+				    <h4 >Password</h4>
+					<input type="password" name="pass"/>
+					<span> <?php echo $err_pass; ?> </span>
+				</div>
+				<br>
+				<div align="center">
+					<input type="submit" name="login" value="Login"/>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ Project/index.php
@@ -0,0 +1,14 @@
+ <html>
+    <head>
+	<title>Login</title>
+	</head>
+	<body>
+	    <div align="center">
+		    <h1>Login</h1><br>
+			<h2>Login as Admin</h2><br>
+			<h2>Login as User</h2><br>
+			<h2>Login as Doctor</h2><br>
+			<h2>Login as Receptionist</h2><br>
+		</div>
+	</body>
+</html>
